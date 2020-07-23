16c1ec5e2891e26431c35b2ef656a530d8173137
linker10 <bilalsharif4@gmail.com>
2020-07-23 06:41:22 +0500

    first commit

---
+++ club/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import ReservationDate,PersonInfo
+# Register your models here.
+
+admin.site.register(ReservationDate)
+admin.site.register(PersonInfo)
